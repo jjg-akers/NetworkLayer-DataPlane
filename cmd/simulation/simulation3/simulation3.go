a22541bd5f1b9653d62735bac0787bbc3baf3704
Joseph-Akers <jjg.akers@gmail.com>
2020-10-25 14:52:33 -0600

    implemented specified network topology

--- cmd/simulation/simulation3/simulation3.go
+++ cmd/simulation/simulation3/simulation3.go
@@ -15,7 +15,7 @@ import (
 var (
 	hostQueueSize   = 1000
 	routerQueueSize = 1000 // 0 means unlimited
-	simulationTime  = 2    // give the network sufficient time to transfer all packets before quitting
+	simulationTime  = 4    // give the network sufficient time to transfer all packets before quitting
 	wg              = sync.WaitGroup{}
 )
 
@@ -27,14 +27,32 @@ func main() {
 	// routerL := []*network.Router{}
 
 	// create network nodes
-	client := network.NewHost(1, hostQueueSize)
-	objectL = append(objectL, client)
+	host1 := network.NewHost(1, hostQueueSize)
+	objectL = append(objectL, host1)
 
-	server := network.NewHost(2, hostQueueSize)
-	objectL = append(objectL, server)
+	host2 := network.NewHost(2, hostQueueSize)
+	objectL = append(objectL, host2)
 
-	routerA := network.NewRouter("A", 1, routerQueueSize)
+	host3 := network.NewHost(3, hostQueueSize)
+	objectL = append(objectL, host3)
+
+	host4 := network.NewHost(4, hostQueueSize)
+	objectL = append(objectL, host4)
+
+	// routerA should have two in, two out interfaces
+	routerA := network.NewRouter("A", 2, routerQueueSize)
 	objectL = append(objectL, routerA)
+
+	// RouterB and C should have only 1 in/ out interface
+	routerB := network.NewRouter("B", 1, routerQueueSize)
+	objectL = append(objectL, routerB)
+
+	routerC := network.NewRouter("C", 1, routerQueueSize)
+	objectL = append(objectL, routerC)
+
+	// routerD should have two in/ out interfaces
+	routerD := network.NewRouter("D", 2, routerQueueSize)
+	objectL = append(objectL, routerD)
 
 	// create a link layer to keep track of links between network nodes
 	linkLayer := link.NewLinkLayer()
@@ -43,9 +61,37 @@ func main() {
 	// add all the links
 	// link paramters ...
 
-	linkLayer.AddLink(link.NewLink(client, 0, routerA, 0, 100))
-	linkLayer.AddLink(link.NewLink(routerA, 0, server, 0, 40))
+	// Add 'in' links to RouterA
+	//	host1 -> interface0
+	linkLayer.AddLink(link.NewLink(host1, 0, routerA, 0, 100))
+	//	host2 -> interface1
+	linkLayer.AddLink(link.NewLink(host2, 0, routerA, 1, 100))
 
+	// Router A -> RouterB
+	//	RouterA out 0 -> RouterB in 0
+	linkLayer.AddLink(link.NewLink(routerA, 0, routerB, 0, 40))
+
+	//Router A -> RouterC
+	//	RouterA out 1 -> RouterC in 0
+	linkLayer.AddLink(link.NewLink(routerA, 1, routerC, 0, 40))
+
+	//Router B -> RouterD
+	//	RouterB out 0 -> RouterD in 0
+	linkLayer.AddLink(link.NewLink(routerB, 0, routerD, 0, 40))
+
+	//RouterC -> RouterD
+	//	RouterC out 0 -> RouterD in 1
+	linkLayer.AddLink(link.NewLink(routerC, 0, routerD, 1, 40))
+
+	//RouterD -> Host3
+	//	RouterD out 0 -> Host3 in 0
+	linkLayer.AddLink(link.NewLink(routerD, 0, host3, 0, 40))
+
+	//RouterD -> Host4
+	//	RouterD out 1 -> Host4 in 0
+	linkLayer.AddLink(link.NewLink(routerD, 1, host4, 0, 40))
+
+	//RouterB -> RouterD
 	// start all the objects
 	for _, obj := range objectL {
 		switch v := obj.(type) {
@@ -68,7 +114,7 @@ func main() {
 	// 	client.UdtSend(2, fmt.Sprintf("Sample data %d", i))
 	// 	i++
 	// }
-	client.UdtSend(2, fmt.Sprintf("0123456789TTTTTTTTTTBBBBB"))
+	host2.UdtSend(2, fmt.Sprintf("0123456789TTTTTTTTTTBBBBB"))
 
 	// give the network sufficient time to transfer all packets before quitting
 	time.Sleep(time.Duration(simulationTime) * time.Second)
